042a69b9644ba5382a45ee4dd5c68eb0a8e95edd
Tsutomu Onoda <tsutomu.onoda@gmail.com>
2018-08-12 12:17:22 +0900

    add key binding settings

--- .emacs.d/conf/init-elpa-company.el
+++ .emacs.d/conf/init-elpa-company.el
@@ -3,5 +3,14 @@
 ;すべてのモードで有効にする場合
 (global-company-mode)
 
-;特定のモードで有効にする場合
+;特定のモードで有効にする場合(例)
 ;(add-hook 'python-mode-hook 'company-mode)
+
+;;キーバインディング
+(define-key company-active-map (kbd "M-n") nil)
+(define-key company-active-map (kbd "M-p") nil)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "C-h") nil)
+
+;;参考URL https://qiita.com/sune2/items/b73037f9e85962f5afb7
