eb541272b384329d8cb5c9b084dc641c55aa7ed4
Tsutomu Onoda <tsutomu.onoda@gmail.com>
2018-11-13 19:45:17 +0900

    comment out marmalade setting

--- .emacs.d/conf/init-package.el
+++ .emacs.d/conf/init-package.el
@@ -1,8 +1,19 @@
 (require 'package)
 
 ;;; Add package-archives
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(add-to-list
+ 'package-archives
+ '("melpa" . "https://melpa.org/packages/"))
+(add-to-list
+ 'package-archives
+ '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+;; SSL証明書の期限切れのため、セキュリィ警告が出る
+;; (add-to-list
+;;  'package-archives
+;;  '("marmalade" . "https://mamalade-repo.org/packages/"))
+
 
 ;; Initialize
 (package-initialize)
