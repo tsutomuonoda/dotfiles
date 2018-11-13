f1c8df19953fbd53031dec9a7c5480e0d6902bae
Tsutomu Onoda <tsutomu.onoda@gmail.com>
2018-11-13 20:14:21 +0900

    company-pycomplete and python-mode setting to mode-hook

--- .emacs.d/conf/22-python.el
+++ .emacs.d/conf/22-python.el
@@ -1,3 +1,25 @@
+;;; 22-python.el --- My Python Settings       -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
 ;; highlight-indentationの設定
 ;(add-hook 'python-mode-hook 'highlight-indentation-mode)
-(add-hook 'python-mode-hook 'highlight-indentation-current-column-mode)
+(add-hook 'python-mode-hook
+	  'highlight-indentation-current-column-mode
+	  '(lambda()
+	     ;; company-pycomplete.el
+	     (setq company-backends '((company-pycomplete)))
+	     ;; python-mode.el
+	     (setq py-highlight-error-source-p t)
+	     (setq py-company-pycomplete-p t)
+	     (setq py-tab-indents-region-p t)
+	     (setq py-verbose-p t)
+	     )
+	  )
+
+(provide '22-python) ;;;
+;;; 22-python.el ends here
+
+
