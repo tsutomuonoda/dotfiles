3d982faa1c9cb57b0f95d207dddfb76946c0a459
Tsutomu Onoda <tsutomu.onoda@gmail.com>
2019-03-31 17:45:02 +0900

    holding settings

--- .emacs.d/conf/init-howm.el
+++ .emacs.d/conf/init-howm.el
@@ -1,7 +1,23 @@
-;;; howm(起動しないので放置)
-;(setq howm-menu-lang 'ja)
-;(global-set-key "\C-c,," 'howm-menu)
-;(autoload 'howm-menu "howm" "Hitori Otegaru Wiki Modoki" t)
+;;; init-howm.el --- My howm Settings       -*- lexical-binding: t; -*-
 
+;;; Commentary:
+;; うまく設定できないので保留
+
+;; Copyright (C) 2018  Tsutomu Onoda
+
+;; Author: Tsutomu Onoda <tsutomu.onoda@gmail.com>
+;; Keywords:
+
+
+
+;;; Code:
+
+;;howm-menuに言語を日本語に
 ;(setq howm-menu-lang 'ja)
-;(require 'howm)
+
+(provide 'init-howm) ;;;
+;;; init-howm.el ends here
+
+
+
+
